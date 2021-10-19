33217701366c8c4dacb71eb08eee6088942e8529
CoryKrol <16892390+CoryKrol@users.noreply.github.com>
2021-10-19 18:50:16 -0500

    Add single source for version (pyproject.toml), move off raw vcrpy and replace with pytest-recording, fix location of cassette files that was causing issues in GitHub actions, move README.md -> README.rst to use with sphinx documentation

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import pkg_resources
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../miningpoolhub_py'))
 
 # -- Project information -----------------------------------------------------
 
@@ -22,7 +22,7 @@ copyright = '2021, Cory Krol'
 author = 'Cory Krol'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.6'
+release = pkg_resources.get_distribution(project).version
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,8 +30,8 @@ release = '0.1.6'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'sphinx.ext.napoleon',
+              'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
