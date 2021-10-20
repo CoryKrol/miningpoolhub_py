239a4710ef5d82bee3c24dc9ed8f58e6c59ecdbb
CoryKrol <16892390+CoryKrol@users.noreply.github.com>
2021-10-19 23:15:34 -0500

    Fix formatting in files, save action for urls as variables to reduce duplication string literals across the module

--- example.py
+++ example.py
@@ -2,10 +2,9 @@ from __future__ import print_function
 from miningpoolhub_py import Pool
 
 pool = Pool('ethereum')
-profit_and_statistics = pool.get_dashboard()
-print(profit_and_statistics)
+profit_and_statistics = pool.get_mining_profit_and_statistics()
 
-# for number, coin in enumerate(profit_and_statistics, start=1):
-#     print("{num}. {name} - {normalized_profit}".format(num=number,
-#                                                        name=coin['coin_name'],
-#                                                        normalized_profit=round(coin['normalized_profit'], 5)))
+for number, coin in enumerate(profit_and_statistics, start=1):
+    print("{num}. {name} - {normalized_profit}".format(num=number,
+                                                       name=coin['coin_name'],
+                                                       normalized_profit=round(coin['normalized_profit'], 5)))
