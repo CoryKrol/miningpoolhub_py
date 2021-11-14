04c02c43533a1b800627162b2afa2bdaffd1c61a
CoryKrol <16892390+CoryKrol@users.noreply.github.com>
2021-11-13 18:26:55 -0600

    Fix path issue for api rate limit file, add exception type for an invalid coin

--- miningpoolhub_py/__init__.py
+++ miningpoolhub_py/__init__.py
@@ -11,8 +11,4 @@ API_KEY = os.environ.get("MPH_API_KEY", None)
 
 from .miningpoolhubapi import MiningPoolHubAPI
 
-# TODO: Possible Errors
-# 1. Bad auth token -> 401 Unauthorized ('Access denied')
-# 2. Success status on auto switching bad -> APIError
-# 4. API Key Rate limit error
-# 5. Need to test calls to API for new account with 0s/nulls in responses -> 200, html document as response
+# TODO: Need to test calls to API for new account with 0s/nulls in responses -> 200, html document as response
